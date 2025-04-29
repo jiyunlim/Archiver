fff0ccd0754a7e2af2e0d5263f0c90061f25f0cb
임지연 <saralimjy@jobkorea.co.kr>
2025-04-29 10:43:08 +0900

    chore: html 정리

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function initFunction() {
   return;
 }
@@ -5,13 +7,32 @@ function initFunction() {
 // Swiper 초기화 함수 따로 분리
 function initSwiper() {
   new Swiper('.main-swiper', {
-    autoplay: true,
-    speed: 1000,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 500,
     loop: true,
-    effect: 'fade',
+    //mousewheel: true,
+    //effect: 'fade',
+    /*
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false
+    },*/
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar"
+    },
+    on: {
+      init: function () {
+        var _elFill = document.querySelector('.swiper-pagination-progressbar span');
+        var _addEl = document.createElement("em");
+        _elFill.append(_addEl);
+      }
     }
   });
+  return;
 }
