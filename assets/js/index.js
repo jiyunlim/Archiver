c367471fd20c616004b5cf3de60d214e911a9d05
임지연 <saralimjy@jobkorea.co.kr>
2025-07-10 17:14:01 +0900

    chore: html 정리

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,28 @@
 'use strict';
 // title 속성 값을 가져와 <h2> 태그 내부에 삽입
 setTimeout(() => {
-  var components = document.querySelectorAll(".component-in"); // 모든 요소 선택
+  var components = document.querySelectorAll(".component-in");
   components.forEach(function (component) {
     var titleValue = component.getAttribute("title"); // title 속성 값 가져오기
-    var heading = component.querySelector("h2.text-title"); // 해당 요소 내 <h2> 찾기
+    var heading = component.querySelector("h2.text-title");
     if (heading && titleValue) {
       heading.textContent = titleValue; // 값 삽입
-    }else { }
+    }
   });
-}, 1500);
+}, 1300);
 
 
 // Swiper 초기화 함수 따로 분리
 function initSwiper() {
   new Swiper('.main-swiper', {
-    slidesPerView: 3,
+    slidesPerView: 4,
     spaceBetween: 30,
     speed: 800,
     loop: true,
-    /**/
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
     autoplay: {
       delay: 4000,
       disableOnInteraction: false
