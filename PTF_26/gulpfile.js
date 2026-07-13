a251b21b710d1cd198c0fa287742db7a95d36c5b
임지연 <ljy_md@mz.co.kr>
2026-07-13 21:36:53 +0900

    feat(page):ptf_26 개선 업로드 - img build

--- PTF_26/gulpfile.js
+++ PTF_26/gulpfile.js
@@ -52,8 +52,7 @@ function minifyJs() {
 // 이미지 복사
 function copyImages() {
   return gulp.src('src/assets/img/**/*')
-    .pipe(gulp.dest('dist/assets/img'))
-    .pipe(gulp.dest('dist/img')); // also copy to dist/img for legacy paths
+    .pipe(gulp.dest('dist/assets/img'));
 }
 
 // Swiper CSS 복사
